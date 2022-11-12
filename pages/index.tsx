b4ca668e5b29ec8affc503a5bd0662231030ca23
Eliberto Silva Castro <elibertocastro@gmai.com>
2022-11-12 13:03:05 -0300

    Removendo dependências inválidas

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Header from "../src/components/Header";
 import Timeline from "../src/components/Timeline";
 import Menu from "../src/components/Menu";
 import config from "../config.json";
-import { Playlist } from "../src/types/playlist";
 import { CSSReset } from "../src/components/CSSReset";
 
 function HomePage() {
